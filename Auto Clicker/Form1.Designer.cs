97840dcf696f2548a6e7202e4855428f8a033db2
yyasharr <yashar_gharabaghian@yahoo.com>
2017-07-14 18:55:51 -0700

    Publishing Version 1.0.0.0

--- Auto Clicker/Form1.Designer.cs
+++ Auto Clicker/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Status = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ClickJobWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(104, 28);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 25);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(183, 28);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 25);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Frequency (S)";
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Location = new System.Drawing.Point(104, 9);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(40, 13);
+            this.Status.TabIndex = 4;
+            this.Status.Text = "Status:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(139, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Stopped";
+            // 
+            // ClickJobWorker
+            // 
+            this.ClickJobWorker.WorkerReportsProgress = true;
+            this.ClickJobWorker.WorkerSupportsCancellation = true;
+            this.ClickJobWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.ClickJobWorker_DoWork);
+            this.ClickJobWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.ClickJobWorker_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(270, 65);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Auto Clicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker ClickJobWorker;
     }
 }
 
--- Auto Clicker/Form1.cs
+++ Auto Clicker/Form1.cs
@@ -7,14 +7,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using System.Runtime.InteropServices;
 
 namespace Auto_Clicker
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        int seconds;
         public Form1()
         {
             InitializeComponent();
+            ClickJobWorker.WorkerReportsProgress = true;
+            ClickJobWorker.WorkerSupportsCancellation = true;
+        }
+
+
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+
+            if (int.TryParse(input, out int n) && n > 0)
+            {
+                seconds = n;
+                label2.Text = "Running";
+                label2.ForeColor = System.Drawing.Color.Green;
+                StartButton.Enabled = false;
+                ClickJobWorker.RunWorkerAsync();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number bigger than 1.");
+            }
+
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            ClickJobWorker.CancelAsync();
+        }
+
+        private void ClickJobWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            label2.Text = "Stopped";
+            label2.ForeColor = System.Drawing.Color.Red;
+            StartButton.Enabled = true;
+        }
+
+        public void ClickJobWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            while (true)
+            {
+                for (int i = 0; i < 2*seconds; i++)
+                {
+                    System.Threading.Thread.Sleep(500); //wait half a second and each time, check if cancel has been clicked
+                    if (ClickJobWorker.CancellationPending == true)
+                    {
+                        ClickJobWorker.CancelAsync();
+                        e.Cancel = true;
+                        return;
+                    }
+                }
+                MouseClicker();
+            }
+            
+
+        }
+        void MouseClicker()
+        {
+            //Call the imported function with the cursor's current position
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
         }
     }
 }
